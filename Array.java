c4c0416b86255457c641084524ccfd60f9a17c91
manjunathan1 <manjunathancnp@gmail.com>
2023-02-26 01:36:52 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,11 @@
+class Array{
+public static void main(String[]args){
+char number[]={'n','a','t','a'};
+int count=0;
+for(int i=0; i<number.length; i++){
+System.out.println(number[i]);
+count=count+1;
+}
+System.out.println(count);
+}
+}
