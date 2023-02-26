1852be566697f29d140cccb0ddeff951abbbd0ab
manjunathan1 <manjunathancnp@gmail.com>
2023-02-26 19:02:25 +0530

    Total

---
+++ Array1_total_length_prints.java
@@ -0,0 +1,10 @@
+class Array1_total_length_prints{
+public static void main(String[]args){
+int []a=new int[2];
+a[0]=100;
+a[1]=200;
+for(int i=0; i<a.length;i++){
+System.out.println(a[i]);
+}
+}
+}
